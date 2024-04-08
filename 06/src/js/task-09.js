216170261ad312925ac8c0cf8d672e99d5d41525
lewypopescu <popesculewy@gmail.com>
2024-04-08 15:33:51 +0300

    done

--- 06/src/js/task-09.js
+++ 06/src/js/task-09.js
@@ -1,18 +1,15 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
-// const colorClick = document.querySelector(".change-color");
-// const showColor = document.querySelector(".color");
+const colorClick = document.querySelector(".change-color");
+const showColor = document.querySelector(".color");
 
-// const colorShow = () => {
-//   document.style.body.background = getRandomHexColor;
-// };
+const colorShow = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  showColor.textContent = getRandomHexColor();
+};
 
-// colorClick.addEventListener("click", colorShow);
-
-// colorClick.addEventListener("input", (event) => {
-//   showColor.textContent = event.currentTarget.value;
-// });
+colorClick.addEventListener("click", colorShow);
